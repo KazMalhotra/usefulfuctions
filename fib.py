89441b07144ef82c70849c097a2b01b7d7ef84e7
Kaz Malhotra <49257206+KazMalhotra@users.noreply.github.com>
2019-12-13 10:29:05 -0800

    Create fib.py

---
+++ fib.py
@@ -0,0 +1,8 @@
+def main():
+    number = eval(input("Enter a non-negative integer:                    "))
+    product = 1
+    for i in range(number):
+        product = product*(i+1)
+    print(product)
+    main()
+main()
